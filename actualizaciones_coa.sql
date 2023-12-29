045dea6ce612b59eebbdc357559e8218a9dfdf8a
Leticia Benítez <leticia@ddsmedia.net>
2023-12-28 22:08:54 -0600

    Guardar resultados

--- actualizaciones_coa.sql
+++ actualizaciones_coa.sql
@@ -8,3 +8,14 @@ CREATE TABLE proveedor_analisis(
 
 ALTER TABLE `proveedor_analisis` ADD CONSTRAINT `fk_proveedor_analisis` FOREIGN KEY (`analisis_id`) REFERENCES `analisis`(`id_analisis`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
 ALTER TABLE `proveedor_analisis` ADD CONSTRAINT `fk_proveedor_id` FOREIGN KEY (`proveedor_id`) REFERENCES `proveedores`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+
+CREATE TABLE proveedor_resultado(
+	id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    proveedor_analisis_id int(11) NOT NULL,
+    lote varchar(100),
+    resultado int(11),
+    estatus varchar(100),
+	comentarios varchar(100)
+);
+
+ALTER TABLE `proveedor_resultado` ADD CONSTRAINT `fk_proveedor_analisis_resultado` FOREIGN KEY (`proveedor_analisis_id`) REFERENCES `proveedor_analisis`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
--- assets/save.php
+++ assets/save.php
@@ -3,26 +3,30 @@
     include("../conexion.php");
 
     // get values 
-    $clave_pt = $_POST['clave_pt'];
-    $pt = $_POST['pt'];    	
-    $caducidad = $_POST['caducidad'];
-    $empaque = $_POST['empaque'];
-    $ehum = $_POST['ehum'];
-    $minh = $_POST['minh'];
-    $maxh = $_POST['maxh'];
-    $eme = $_POST['eme'];
-    $eg8 = $_POST['eg8'];
-    $e30 = $_POST['e30'];
-    $efo = $_POST['efo'];
-    $eph = $_POST['eph'];
+    $_POST = json_decode(file_get_contents('php://input'), true);
+    $lote = $_POST['lote']; $estatus = $_POST['estatus']; $comentarios = $_POST['comentarios'];
+    foreach($_POST['arregloResultados'] as $resultado){
+        $proveedor_analisis_id = $resultado['id_proveedor_analisis']; 
+        $res_val = $resultado['valorResultado'];
+        try {
+            $insert_resultado = "INSERT INTO proveedor_resultado(proveedor_analisis_id, lote, resultado, estatus, comentarios) VALUES($proveedor_analisis_id, '$lote', $res_val, '$estatus', '$comentarios');"; 
+            $resultado_insert = mysqli_query($conexion, $insert_resultado);
+        } catch (\Throwable $th) {
+            // throw $th;
+            echo json_encode(['success' => false, 'error' => $th]);
+        }
+    }
+
+    if($resultado_insert) echo json_encode(['success' => true]);
+    else echo json_encode(['success' => false, 'error' => 'No se agregó el resultado de análisis']);
     
                
-        $query1 = "INSERT INTO p_hum(minh,maxh,ehum) VALUES('$minh','$maxh','$ehum')";
-        $query = "INSERT INTO claves(clave_pt,pt,caducidad,empaque) VALUES('$clave_pt','$pt','$caducidad','$empaque')";
-        if (!$result = mysqli_query($conexion, $query)) {
-            exit(mysqli_error($conexion));
+    //     $query1 = "INSERT INTO p_hum(minh,maxh,ehum) VALUES('$minh','$maxh','$ehum')";
+    //     $query = "INSERT INTO claves(clave_pt,pt,caducidad,empaque) VALUES('$clave_pt','$pt','$caducidad','$empaque')";
+    //     if (!$result = mysqli_query($conexion, $query)) {
+    //         exit(mysqli_error($conexion));
         
-        }
-        echo '<script language="javascript">alert("PT añadido con éxito!");</script>';
-        header('location: ../lotes.php');
+    //     }
+    //     echo '<script language="javascript">alert("PT añadido con éxito!");</script>';
+    //     header('location: ../lotes.php');
 ?>
